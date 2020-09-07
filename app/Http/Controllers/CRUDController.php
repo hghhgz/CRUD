cf43db32c45707af38b962fd002a76c4d2727435
hghhgz <hghhgz12@gmail.com>
2020-09-07 20:07:23 +0300

    CRUD

---
+++ app/Http/Controllers/CRUDController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CRUD;
+use Illuminate\Http\Request;
+
+class CRUDController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CRUD  $cRUD
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CRUD $cRUD)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CRUD  $cRUD
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CRUD $cRUD)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CRUD  $cRUD
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CRUD $cRUD)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CRUD  $cRUD
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CRUD $cRUD)
+    {
+        //
+    }
+}
---
+++ database/factories/CRUDFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\CRUD;
+use Faker\Generator as Faker;
+
+$factory->define(CRUD::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
