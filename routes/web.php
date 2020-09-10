fc91a81e1c8b104dc8b72ca33267e8462c5e15dd
hghhgz <hghhgz12@gmail.com>
2020-09-10 09:35:35 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('blogs','blogsController');
+Route::resource('blogs','blogController');
  
